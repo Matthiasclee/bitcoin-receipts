7a6a4956001c8c855bb8e6f94250fa58457247b6
Matthias Lee <matthias@matthiasclee.com>
2023-05-12 16:39:46 -0700

    Use real data

--- generate-receipt.rb
+++ generate-receipt.rb
@@ -1,3 +1,6 @@
+require "json"
+require "net/http"
+
 def receipt(price, fees)
   d="#{Time.now.strftime("%a, %b %e, %Y at%l:%M %p %Z")}"
 
@@ -10,10 +13,22 @@ def receipt(price, fees)
   "\
   Bitcoin Data#{" " * (47-12-d.length)}#{d}\n\
   #{?=*47}\n\
-  Price#{" " * (47-6-p.length)}#{p}\n\n\
+  Price#{" " * (47-5-p.length)}#{p}\n\n\
   Fastest TX fee#{" " * (47-14-fh.length)}#{fh}\n\
   Half hour TX fee#{" " * (47-16-fm.length)}#{fm}\n\
   One hour TX fee#{" " * (47-15-fl.length)}#{fl}"
 end
 
-puts receipt("25,000", [1000,500,50])
+# Get data
+price = Net::HTTP.get(URI("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD"))
+price = JSON.parse(price)["USD"].to_i.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
+
+fees = Net::HTTP.get(URI("https://bitcoinfees.earn.com/api/v1/fees/recommended"))
+fees = JSON.parse(fees)
+
+
+puts receipt(price,
+            [fees["fastestFee"],
+            fees["halfHourFee"],
+            fees["hourFee"]]
+            )
